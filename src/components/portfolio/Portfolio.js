848a337ab7fb9add79fa2c2fa5ed734419f54289
Shivli Agrawal <shivliagrawal@pal-nat186-181-222.itap.purdue.edu>
2024-12-02 18:35:06 -0500

    new

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -5,33 +5,46 @@ import { info } from "../../info/Info";
 
 export default function Portfolio({ innerRef }) {
     return (
-        <Box id="portfolio" ref={innerRef}>
+        <Box
+            id="portfolio"
+            ref={innerRef}
+            style={{
+                width: '800px',
+                height: '600px',
+                padding: '1rem', // Add padding to prevent content cutoff
+                boxSizing: 'border-box', // Ensure padding is included in dimensions
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
             <Grid
                 container
                 justifyContent="center"
                 alignItems="center"
                 style={{
-                    width: '800px',
-                    height: '600px',
-                    display: 'flex',
-                    flexDirection: 'column', // Arrange rows
+                    width: '100%',
+                    height: '100%',
                 }}
             >
                 {/* Top Row */}
                 <Grid
                     container
                     justifyContent="center"
-                    style={{ width: '100%', display: 'flex', marginBottom: '1rem' }}
+                    style={{
+                        width: '100%',
+                        display: 'flex',
+                        marginBottom: '1rem',
+                    }}
                 >
                     {info.portfolio.slice(0, 2).map((project, index) => (
                         <Grid
                             item
-                            xs={12}
-                            md={6}
                             key={index}
                             style={{
                                 width: '45%',
-                                height: '45%',
+                                height: '40%',
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
@@ -52,17 +65,19 @@ export default function Portfolio({ innerRef }) {
                 <Grid
                     container
                     justifyContent="center"
-                    style={{ width: '100%', display: 'flex', marginTop: '1rem' }}
+                    style={{
+                        width: '100%',
+                        display: 'flex',
+                        marginTop: '1rem',
+                    }}
                 >
                     {info.portfolio.slice(2, 4).map((project, index) => (
                         <Grid
                             item
-                            xs={12}
-                            md={6}
                             key={index}
                             style={{
                                 width: '45%',
-                                height: '45%',
+                                height: '40%',
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
