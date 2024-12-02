b9334b28dd4ab7ecc76c312173973873400ef7b4
Shivli Agrawal <shivliagrawal@pal-nat186-181-222.itap.purdue.edu>
2024-12-02 17:02:16 -0500

    new

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,16 +3,25 @@ import PortfolioBlock from "./PortfolioBlock";
 import {Box, Grid} from "@mui/material";
 import {info} from "../../info/Info";
 
-export default function Portfolio({innerRef}) {
+export default function Portfolio({ innerRef }) {
     return (
-        <Box id={'portfolio'} ref={innerRef}>
-            <Grid container display={'inline-block'} justifyContent={'center'}>
+        <Box id="portfolio" ref={innerRef}>
+            <Grid
+                container
+                justifyContent="center"
+                style={{ width: '800px', height: '600px' }} // Customize dimensions here
+            >
                 {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
-                       <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
-                   </Grid>
+                    <Grid item xs={12} md={6} key={index}>
+                        <PortfolioBlock
+                            image={project.image}
+                            live={project.live}
+                            source={project.source}
+                            title={project.title}
+                        />
+                    </Grid>
                 ))}
             </Grid>
         </Box>
     );
-};
+}
